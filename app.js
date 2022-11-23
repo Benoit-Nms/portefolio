cf19dc1a7ead1fd2181f4c01a1e4c84bd3a889cc
Benoît Namous <84500783+Benoit-Nms@users.noreply.github.com>
2022-11-23 03:09:25 +0100

    ajout symboles dark mode

--- app.js
+++ app.js
@@ -15,13 +15,16 @@ function changeDarkMode() {
         document.documentElement.style.setProperty("--text-color", "black");
         document.documentElement.style.setProperty("--background-color", "#d4d7ff");
         document.documentElement.style.setProperty("--project-bg-color", "#C2C1FF");
-        document.getElementById("dark-light-mode").innerHTML = "Mode sombre";
+        document.getElementById("dark-light-mode").innerHTML = "Mode sombre ☾";        
     } else {
         // dark mode
         darkMode = true;
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--background-color", "#10042A");
         document.documentElement.style.setProperty("--project-bg-color", "#1e1d46");                
-        document.getElementById("dark-light-mode").innerHTML = "Mode clair";    
+        document.getElementById("dark-light-mode").innerHTML = "Mode clair ☼";    
     }        
 }
+
+
+
