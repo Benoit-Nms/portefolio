71df6c484c36b1e2c03249bbba72d80fae2cfad5
Benoît Namous <84500783+Benoit-Nms@users.noreply.github.com>
2022-12-12 01:28:24 +0100

    ajout flèche pour remonter la page

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let darkMode = false;
 let emailCopy = document.querySelector("#email");
-    emailCopy.addEventListener('click', () => {        
+    emailCopy.addEventListener('click', () => {
   })
   ;
-    emailCopy.addEventListener('click', () => {        
+    emailCopy.addEventListener('click', () => {
     navigator.clipboard.writeText(emailCopy.innerText);
-    emailCopy.innerText = "L'e-mail a bien été copié !";    
+    emailCopy.innerText = "L'e-mail a bien été copié !";
   });
 
 function changeDarkMode() {
@@ -15,16 +15,23 @@ function changeDarkMode() {
         document.documentElement.style.setProperty("--text-color", "black");
         document.documentElement.style.setProperty("--background-color", "#d4d7ff");
         document.documentElement.style.setProperty("--project-bg-color", "#C2C1FF");
-        document.getElementById("dark-light-mode").innerHTML = "Mode sombre ☾";        
+        document.getElementById("dark-light-mode").innerHTML = "Mode sombre ☾";
     } else {
         // dark mode
         darkMode = true;
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--background-color", "#10042A");
-        document.documentElement.style.setProperty("--project-bg-color", "#1e1d46");                
-        document.getElementById("dark-light-mode").innerHTML = "Mode clair ☼";    
-    }        
+        document.documentElement.style.setProperty("--project-bg-color", "#1e1d46");
+        document.getElementById("dark-light-mode").innerHTML = "Mode clair ☼";
+    }
 }
 
+const btn = document.querySelector('.btn-up');
 
-
+btn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  })
+})
